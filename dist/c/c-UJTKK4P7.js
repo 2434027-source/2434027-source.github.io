82654bdab01186d74bb6e2afc6f87d27809ab728
Tigran <2434027@gmail.com>
2026-01-28 20:43:43 +0300

    Old-site

--- dist/c/c-UJTKK4P7.js
+++
@@ -1,1 +0,0 @@
-﻿var e=(r,n)=>new Intl.NumberFormat(window.navigator.language||"en-US",{style:"currency",currency:n}).format(r);export{e as a};
---
+++ dist/c/c-YJ3QW6TZ.js
@@ -0,0 +1,1 @@
+import{a as e}from"https://st-p.rmcdn1.net/22ac7430/dist/c/c-DSUHDIPY.js";var t=e(()=>{"use strict"});export{t as a};
